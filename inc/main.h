714cc7fb47bd7f0348ae32e59377e4274e4d90f7
activiteOCR <julien.terrier3@wanadoo.fr>
2021-02-21 13:15:24 -0500

    Add src and inc files

---
+++ inc/main.h
@@ -0,0 +1,6 @@
+#ifndef H_main
+#define H_main
+
+/*Add here structures and protoypes*/
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,12 @@
+/*Test github */
+
+#include <stdio.h>
+#include "../inc/main.h"
+
+
+int main ()
+{
+
+	printf ("Plz Add hereinatter your full name with printf");
+
+}
